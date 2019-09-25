40b4bd1593ebc4210b994bba997a953a7896dad4
Carmen SanDiego <efemar82@gmail.com>
2019-09-25 15:20:33 -0700

    added date conversion + default movie

--- liri.js
+++ liri.js
@@ -6,6 +6,8 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var axios = require("axios");
 var Spotify = require('node-spotify-api');
+var fs = require("fs")
+var moment = require("moment")
 
 //how to call the spotify and OMDB key
 
@@ -14,7 +16,17 @@ var Spotify = require('node-spotify-api');
 var command = process.argv[2];
 var artist = process.argv.slice(3).join("-");
 var movieName = process.argv.slice(3).join("-");
+
+if (!movieName) {
+    movieName = "Mr. Nobody";
+}
+
 var songName = process.argv.slice(3).join(" ");
+
+if (!songName) {
+    movieName = "The Sign";
+}
+
 
 //TODO: Still NOT WORKING
 var bandsintownQuery = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
@@ -32,7 +44,21 @@ switch (command) {
         //calls a request from Bands in Town
         axios.get(bandsintownQuery).then(
             function (response) {
-                console.log(JSON.stringify(response.data[0].venue.name, null, 2));
+                //console.log(JSON.stringify(response.data, null, 2));
+                console.log("Venue Name: " + response.data[0].venue.name)
+                console.log("Venue Location: " + response.data[0].venue.city)
+                console.log("Date of Event: " + moment(response.data[0].datetime, moment.ISO_8601).format("MM/DD/YY"))
+
+                // var jsonData = response.data[0];
+
+                // var bands = [
+                //     "Name: " + jsonData.venue.name,
+                //     "Venue Location: " + jsonData.venue.city,
+                //     "Date of Event: " + moment(jsonData.datetime, "MM/DD/YYYY")
+                // ]
+
+                // console.log(bands)
+
             })
             .catch(function (error) {
                 if (error.response) {
@@ -58,22 +84,24 @@ switch (command) {
 
 
 
-
-
-
-
-
-
         break;
 
     //movie-this '<movie name here>'
     //calls a request from OMDB
     case "movie-this":
+        
         axios.get(omdbQuery)
             .then(
                 function (response) {
                     console.log(JSON.stringify(response.data, null, 2));
-
+                    console.log("Title: " + response.data.Title)
+                    console.log("Year: " + response.data.Year)
+                    console.log("IMBD Rating: " + response.data.imdbRating)
+                    console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value)
+                    console.log("Country where it was produced: " + response.data.Ratings[1].Value)
+                    console.log("Language: " + response.data.Language)
+                    console.log("Plot: " + response.data.Plot)
+                    console.log("Actors: " + response.data.Actors)
 
 
                 })
@@ -124,8 +152,12 @@ switch (command) {
                 console.log(err);
             });
 
-    //output is: artist, the song's name, a preview link of the song, the album that the song is from
+        //output is: artist, the song's name, a preview link of the song, the album that the song is from
 
+        break;
+
+    case "do-what-it-says":
+    //call the random.txt file
 
 
 
@@ -144,10 +176,5 @@ switch (command) {
 
 
 
-
-
-
-//do-what-it-says
-//call the random.txt file
 
 
