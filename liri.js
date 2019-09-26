ba021917c0ea32b7b9a0d0d2e79301cf7d9a63fe
Carmen SanDiego <efemar82@gmail.com>
2019-09-25 22:11:16 -0700

    added spotify search

--- liri.js
+++ liri.js
@@ -1,4 +1,9 @@
 
+//TODO: 
+//Loop through Cher's concert dates
+//Create a video or giphy for the commands
+
+
 //TODO: install de dotenv package
 require("dotenv").config();
 
@@ -8,10 +13,6 @@ var axios = require("axios");
 var Spotify = require('node-spotify-api');
 var fs = require("fs")
 var moment = require("moment")
-
-//how to call the spotify and OMDB key
-
-// var omdb = new OMDB(keys.omdb);
 
 var command = process.argv[2];
 var artist = process.argv.slice(3).join("-");
@@ -23,41 +24,126 @@ if (!movieName) {
 
 var songName = process.argv.slice(3).join(" ");
 
-if (!songName) {
-    movieName = "The Sign";
-}
 
 
-//TODO: Still NOT WORKING
+
 var bandsintownQuery = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-//console.log(bandsintownURL);
-
 var omdbQuery = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-//console.log(omdbURL);
+
+
+
 
 //var spotifyQuery = 'All the Small Things'
 
+function searchSpotifyNoTrack() {
+
+    var spotify = new Spotify({
+        id: keys.spotify.id,
+        secret: keys.spotify.secret
+    });
+    spotify
+        .search({ type: "track", query: "The Sign" })
+        .then(function (response) {
+
+            console.log(response)
+            //console.log(JSON.stringify(response.tracks))
+            //console.log ("Artist: " + response.tracks.items)
+            // console.log("Song: " + songName) 
+            // console.log("Url: " + response.tracks.items[0].album.external_urls.spotify)
+            // console.log("Album Name: " + response.tracks.items[0].album.name)
+            // console.log("\n-----------------\n")
+
+
+            // if (response.tracks.items.length === 0) {
+            //     songName = "The Sign"
+            // }
+
+            // for (var i = 0; i < response.tracks.items.length; i++) {
+            //     printTrack(response.tracks.items[i])
+            // }
+
+            // function printTrack() {
+            //     var artist = response.tracks.items[5].album.artists[0].name
+            //     var song = songName
+            //     var spotifyUrl = response.tracks.items[5].album.external_urls.spotify
+            //     var albumName = response.tracks.items[5].album.name
+            //     console.log("Artist: " + artist)
+            //     console.log("Song: " + song)
+            //     console.log("Url: " + spotifyUrl)
+            //     console.log("Album Name: " + albumName)
+            //     console.log("\n-----------------\n")
+            // }
+            // printTrack();
+
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+
+
+function searchSpotify(songName) {
+    var spotify = new Spotify({
+        id: keys.spotify.id,
+        secret: keys.spotify.secret
+    });
+    spotify
+        .search({ type: 'track', query: songName })
+        .then(function (response) {
+
+            console.log(JSON.stringify(response.tracks.items.length))
+            //console.log(JSON.stringify(response.tracks.items[0], null, 2))
+            //console.log(response.tracks)
+            // console.log ("Artist: " + response.tracks.items[0].album.artists[0].name)
+            // console.log("Song: " + songName)
+            // console.log("Url: " + response.tracks.items[0].album.external_urls.spotify)
+            // console.log("Album Name: " + response.tracks.items[0].album.name)
+            // console.log("\n-----------------\n")
+
+
+            // if (response.tracks.items.length === 0) {
+            //     songName = "The Sign"
+            // }
+
+            for (var i = 0; i < response.tracks.items.length; i++) {
+                printTrack(response.tracks.items[i])
+            }
+
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+
+function printTrack(track) {
+    var artist = track.album.artists[0].name
+    var song = songName
+    var spotifyUrl = track.album.external_urls.spotify
+    var albumName = track.album.name
+    console.log("Artist: " + artist)
+    console.log("Song: " + song)
+    console.log("Url: " + spotifyUrl)
+    console.log("Album Name: " + albumName)
+    console.log("\n-----------------\n")
+}
+
+
 switch (command) {
     case "concert-this":
-
-        // concert-this <artist/band name here>`
-        //calls a request from Bands in Town
         axios.get(bandsintownQuery).then(
             function (response) {
                 //console.log(JSON.stringify(response.data, null, 2));
-                console.log("Venue Name: " + response.data[0].venue.name)
-                console.log("Venue Location: " + response.data[0].venue.city)
-                console.log("Date of Event: " + moment(response.data[0].datetime, moment.ISO_8601).format("MM/DD/YY"))
 
-                // var jsonData = response.data[0];
+                //var band = response.data[0]
 
-                // var bands = [
-                //     "Name: " + jsonData.venue.name,
-                //     "Venue Location: " + jsonData.venue.city,
-                //     "Date of Event: " + moment(jsonData.datetime, "MM/DD/YYYY")
-                // ]
+                for (var i = 0; i < response.data.length; i++) {
+                    console.log("Band's Name: " + artist)
+                    console.log("Venue Name: " + response.data[i].venue.name)
+                    console.log("Venue Location: " + response.data[i].venue.city)
+                    console.log("Date of Event: " + moment(response.data[i].datetime, moment.ISO_8601).format("MM/DD/YYYY"))
+                    console.log("\n-----------------\n")
+                }
 
-                // console.log(bands)
 
             })
             .catch(function (error) {
@@ -80,16 +166,9 @@ switch (command) {
                 }
                 console.log(error.config);
             });
-        //cames back with a response with name of venue, venue location, date of the event (use moment for this)
-
-
-
         break;
 
-    //movie-this '<movie name here>'
-    //calls a request from OMDB
     case "movie-this":
-        
         axios.get(omdbQuery)
             .then(
                 function (response) {
@@ -106,66 +185,37 @@ switch (command) {
 
                 })
             .catch(function (error) {
-                if (error.response) {
-                    // The request was made and the server responded with a status code
-                    // that falls out of the range of 2xx
-                    console.log("---------------Data---------------");
-                    console.log(error.response.data);
-                    console.log("---------------Status---------------");
-                    console.log(error.response.status);
-                    console.log("---------------Status---------------");
-                    console.log(error.response.headers);
-                } else if (error.request) {
-                    // The request was made but no response was received
-                    // `error.request` is an object that comes back with details pertaining to the error that occurred.
-                    console.log(error.request);
-                } else {
-                    // Something happened in setting up the request that triggered an Error
-                    console.log("Error", error.message);
-                }
-                console.log(error.config);
+                console.log(error);
             });
-
-
-        //output: title, year, IMBDB, rotten, country, language, plot, actors
-        //if no movie is defined, "Mr Nobody is the default
-
-
-
         break;
 
     case "spotify-this-song":
-        //spotify-this-song '<song name here>'
-        //calls a request from spotify
 
-        var spotify = new Spotify({
-            id: keys.spotify.id,
-            secret: keys.spotify.secret
-        });
+        if (!songName) {
+            songName = "The Sign";
+            artist = "Ace of Base",
+                searchSpotifyNoTrack(songName, artist);
+        } else {
+            searchSpotify(songName)
+        }
 
-        spotify
-            .search({ type: 'track', query: songName })
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
-
-        //output is: artist, the song's name, a preview link of the song, the album that the song is from
-
+    
         break;
 
     case "do-what-it-says":
-    //call the random.txt file
+        //call the random.txt file
+        fs.readFile("random.txt", "utf8", function (err, data) {
+            if (err) {
+                return console.log(err);
+            }
+            data = data.split(", ");
+
+            spotify(data[1]);
 
 
-
-
-
-
-
+        })
 }
+
 
 
 
